2d134902a5e35b026300ccf34cc3450f5ad3b00b
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2025-12-23 18:40:37 +0530

    Update cloudinary.ts

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name:"dg3m5p9xc",
-  api_key: "284118412187186",
-  api_secret: "d3PJmL1zQQtjo6_uvCGN3OxTRbI",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
